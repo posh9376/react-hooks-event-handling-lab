5ee44c2efd5bc968ba0f57e59675eaacdd0cf2eb
Eric Richard <reric0802@gmail.com>
2024-11-20 04:17:54 +0300

    created keypad and eyes on me components

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    const change = (event) => {
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={change}/>
+        </div>
     )
 }
 
